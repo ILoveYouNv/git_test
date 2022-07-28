2cf60c5a3dc96c3cf6707245fdb2415720546e0e
XueFeng <XueFeng@Javalearning.com>
2022-07-28 20:27:05 +0800

    third commit

--- src/main/java/com/xuefeng/git/GitTest.java
+++ src/main/java/com/xuefeng/git/GitTest.java
@@ -8,6 +8,7 @@ package com.xuefeng.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!3");
         System.out.println("hello git!2");
     }
 }
