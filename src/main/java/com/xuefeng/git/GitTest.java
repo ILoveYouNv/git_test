bc96ba14b7b4970a9a954581b7b830bc3f7cd1cd
XueFeng <XueFeng@Javalearning.com>
2022-07-28 20:02:17 +0800

    hot-fix commit1

--- src/main/java/com/xuefeng/git/GitTest.java
+++ src/main/java/com/xuefeng/git/GitTest.java
@@ -8,6 +8,6 @@ package com.xuefeng.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git!1");
+        System.out.println("hello git!2");
     }
 }
