d36962a3aa747d93a655fb55cbd1cc966d741766
XueFeng <XueFeng@Javalearning.com>
2022-07-28 19:38:23 +0800

    first commit

---
+++ src/main/java/com/xuefeng/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xuefeng.git;
+
+/**
+ * @author xuefeng
+ * @creat 2022-07-28 11:20
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
